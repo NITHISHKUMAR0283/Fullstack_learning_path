15437f210c3e304f9823f735e32e8e4fe0a5a0ef
NITHISHKUMAR0283 <nk0283@srmist.edu.in>
2025-08-13 09:32:52 +0530

    Created form and styled

--- js/practicing/script.js
+++ js/practicing/script.js
@@ -12,3 +12,35 @@ function addoptions(ele){
     select.appendChild(option)
     
 }
+let category_select=document.createElement('select')
+category_select.id='category_select'
+let category_list=[
+    "Food & Dining",
+    "Transportation", 
+    "Shopping",
+    "Entertainment",
+    "Bills & Utilities",
+    "Healthcare",
+    "Salary",
+    "Freelance",
+    "Investment",
+    "Other"
+];
+category_list.forEach(element=>{
+    let category_slides=document.createElement('option')
+    category_slides.value=element
+    category_slides.textContent=element
+    category_select.appendChild(category_slides)
+})  
+let categrory_label=document.createElement('label')
+
+categrory_label.textContent='Category'
+categrory_label.htmlFor='category_select'
+let div_category=document.createElement('div')
+div_category.id='form-div'
+
+div_category.appendChild(categrory_label)
+div_category.appendChild(category_select)
+console.log(div_category)
+document.querySelector('#form-div:nth-child(3)').insertAdjacentElement('afterend',div_category)
+console.log(category_select)
